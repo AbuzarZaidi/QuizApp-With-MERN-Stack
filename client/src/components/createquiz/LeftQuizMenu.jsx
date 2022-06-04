12e7c329cca8a22820691a8722ad0828f4964669
AbuzarZaidi <abuzarzaidi947@gmail.com>
2022-06-05 00:56:18 +0500

    work on left quiz

--- client/src/components/createquiz/LeftQuizMenu.jsx
+++ client/src/components/createquiz/LeftQuizMenu.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
-import { styled } from "@mui/material/styles";
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 
 function Item(props) {
   const { sx, ...other } = props;
@@ -39,17 +39,18 @@ const LeftQuizMenu = () => {
   return (
     <>
     
-      <div style={{ width: '100%', height: 360 }}>
+      <div style={{ width: '100%', height: 360}}>
       <Box
         sx={{
           display: 'grid',
           gridAutoRows: '90px',
           gap: 7,
+          mt:4
         }}
       >
         <Item sx={{ gridColumn: '1', gridRow: 'span 2' }}> 
-        <Button variant="outlined" sx={{ fontWeight: 'bold', textTransform: 'capitalize',px:4,py:2,mt:3,color:"black",backgroundColor:"#F2F2F2"}}>Quiz Summary</Button>
-        <Button variant="contained" sx={{ fontWeight: 'bold', textTransform: 'capitalize',px:5,mt:3}}>Add Question</Button>
+        <Button variant="outlined" sx={{ fontWeight: 'bold', textTransform: 'capitalize',px:3,py:2,mt:3,color:"black",backgroundColor:"#F2F2F2"}}><SettingsOutlinedIcon/>Quiz Summary</Button>
+        <Button variant="contained" sx={{ fontWeight: 'bold', textTransform: 'capitalize',px:5,py:2,mt:3}}>Add Question</Button>
         </Item>
         {/* The second non-visible row has height of 40px */}
         <Item sx={{ gridColumn: '1', gridRow: '4 / 5' }}>
