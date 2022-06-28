0fedd4b2172a4452731926f61767865b23500d3a
AbuzarZaidi <abuzarzaidi947@gmail.com>
2022-06-29 01:31:29 +0500

    commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,28 +1,9 @@
 import React from 'react'
-import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import CardMedia from '@mui/material/CardMedia';
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
+
 const Home = () => {
   return (
     <>
